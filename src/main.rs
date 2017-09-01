f6ccc83d32221599ff8897927a64e9f51dfe6fac
BookOwl <stanleybookowl@gmail.com>
2017-09-01 16:08:05 -0400

    Start work on the command executer

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,8 @@ extern crate rustyline;
 extern crate combine;
 use combine::combinator as cc;
 use combine::Parser;
-
 use std::iter::Iterator;
+use std::process;
 use rustyline::completion::FilenameCompleter;
 
 /// A CmdReader allows iterating over standard input
@@ -38,21 +38,37 @@ impl Iterator for CmdReader {
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
-enum Ast {
-    Cmd(Vec<String>),
+enum Command {
+    Command(Vec<String>),
+}
+impl Command {
+    fn execute(&self) -> Result<std::process::Child, String> {
+        match *self {
+            Command::Command(ref args) => {
+                if let Some((cmd, args)) = args.split_first() {
+                    process::Command::new(cmd)
+                                     .args(args)
+                                     .spawn()
+                                     .map_err(|e| format!("{}", e))
+                } else {
+                    Err(format!("Invalid command {:?}", args))
+                }
+            }
+        }
+    }
 }
 
-fn parse_cmd(line: &str) -> Ast {
+fn parse_cmd(line: &str) -> Command {
     let any_nonwhitespace = || cc::satisfy(|c: char| !c.is_whitespace());
     let arg = || cc::many1::<String, _>(any_nonwhitespace()).skip(combine::char::spaces());
-    let cmd = || cc::many1::<Vec<_>, _>(arg()).map(|args| Ast::Cmd(args));
+    let cmd = || cc::many1::<Vec<_>, _>(arg()).map(|args| Command::Command(args));
     (cmd(), cc::eof()).parse(combine::State::new(line)).map(|((x, _), _)| x).unwrap()
 }
 
 fn main() {
     let cmds = CmdReader::new("> ");
     for line in cmds {
-        let ast = parse_cmd(&line);
-        println!("{:?}", ast)
+        let command = parse_cmd(&line);
+        command.execute().unwrap().wait().unwrap();
     }
 }
