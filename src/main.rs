df8280d8c0a3e0410f5f5bd36934465b4e5fe518
BookOwl <stanleybookowl@gmail.com>
2017-09-01 14:50:32 -0400

    Add the input reading iterator

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
+extern crate rustyline;
+
+use std::iter::Iterator;
+use rustyline::completion::FilenameCompleter;
+
+/// A CmdReader allows iterating over standard input
+/// without having to manually manage the rustyline::Editor's history.
+struct CmdReader {
+    rl: rustyline::Editor<FilenameCompleter>,
+    prompt: &'static str,
+}
+impl CmdReader {
+    /// Creates a new CmdReader that uses the supplied prompt.
+    fn new(prompt: &'static str) -> CmdReader {
+        let mut rl = rustyline::Editor::new();
+        rl.set_completer(Some(FilenameCompleter::new()));
+        CmdReader {
+            rl,
+            prompt,
+        }
+    }
+}
+impl Iterator for CmdReader {
+    type Item = String;
+
+    fn next(&mut self) -> Option<String> {
+        match self.rl.readline(self.prompt) {
+            Ok(line) => {
+                self.rl.add_history_entry(&line);
+                Some(line)
+            },
+            Err(_) => None
+        }
+    }
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let cmds = CmdReader::new("> ");
+    for line in cmds {
+        println!("{}", line)
+    }
 }
